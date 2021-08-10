b6673a0c119455a3a619fb4f9e023ea6cf6d7506
sumit singh <knowledgehomegrown@gmail.com>
2021-08-10 22:05:33 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,275 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="index.css" type="text/css">
+
+    <title>Clone Of Internshala</title>
+  </head>
+  <body>
+  <nav  class=" navbar navbar-expand-lg sticky-top navbar-light bg-light">
+    <div class="container">
+
+  <a class="navbar-brand" href="index.php"><b>Internshala</b></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+
+  <div class="collapse navbar-collapse " id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+    <li class="nav-item pl-3">
+        <a class="nav-link" href="#about">Internships</a>
+      </li>
+      <li class="nav-item pl-3 pt-2">
+
+      <div class="dropdown m-0 ">
+  <a class=" dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+  Online Tranings
+</a>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+  <a class="dropdown-item active" href="#">Programming</a>
+  <a class="dropdown-item" href="#">Business & Management</a>
+  <a class="dropdown-item" href="#">Core Engneering</a>
+  <a class="dropdown-item" href="#">Data science</a>
+  <a class="dropdown-item" href="#">Design</a>
+  <a class="dropdown-item" href="#">Creative Arts</a>
+  <a class="dropdown-item" href="#">Language</a>
+  <a class="dropdown-item" href="#">Career Development</a>
+  <a class="dropdown-item" href="#">Arcticture</a>
+  <a class="dropdown-item" href="#">Specializations</a>
+  </div>
+</div>
+
+        <!-- <a class="nav-link" href="#contact">Online Tranings</a> -->
+      </li>
+      <li class="nav-item pl-3">
+      <a class="nav-link" href="#contact">Fresher Jobs</a>
+      </li>
+
+      <li class="nav-item pl-3">
+      <button type="button" class="btn btn-outline-primary btn-sm mt-1 pt-1 pb-1 pl-4 pr-4">Login</button>
+      </li>
+
+      <li class="nav-item pl-3 dropdown pb-0">
+
+      <!-- <div class="dropdown pb-0"> -->
+  <button class="btn btn-sm btn-secondary dropdown-toggle mt-1 pt-1 pb-1 pl-4 pr-4" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Register
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">As a student</a>
+    <a class="dropdown-item" href="#">As an employer</a>
+  <!-- </div> -->
+</div>
+
+      <!-- <a class="nav-link" href="#contact">Register</a> -->
+      </li>
+
+      </ul>
+  </div>
+</div>
+</nav> 
+
+<div class="container mt-4 center">
+<form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+</div>
+
+<!-- <div class="carousel1"> -->
+<div id="carouselExampleIndicators" class="carousel slide carousel1 mt-3" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
+  </ol>
+  <div class="carousel-inner ">
+    <div class="carousel-item active carousel1 pl-3 pr-3 ">
+      <img src="img/06_Internshala_and_internships-1200x628.jpg" class="d-block w-100 carousel1" alt="...">
+    </div>
+    <div class="carousel-item carousel1 pl-3 pr-3">
+      <img src="img/images (1).jpg" class="d-block w-100 carousel1" alt="...">
+    </div>
+    <div class="carousel-item carousel1 pl-3 pr-3">
+      <img src="img/images.jpg" class="d-block w-100 carousel1" alt="...">
+    </div>
+    <div class="carousel-item carousel1 pl-3 pr-3">
+      <img src="img/Internshala-Training.jpg" class="d-block w-100 carousel1" alt="...">
+    </div>
+    <div class="carousel-item carousel1 pl-3 pr-3">
+      <img src="img/internshala_og_image.jpg" class="d-block w-100 carousel1" alt="...">
+    </div>
+    <div class="carousel-item carousel1 pl-3 pr-3">
+      <img src="img/internship-concept-man-holding-tablet-260nw-348916589.jpg" class="d-block w-100 carousel1" alt="...">
+    </div>
+    <div class="carousel-item carousel1 pl-3 pr-3">
+      <img src="img/Internshala-Training.jpg" class="d-block w-100 carousel1" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<div class="container mt-4 mb-4">
+  <h2 >Popular cities</h2>
+  </div>
+<div class="container mt-3">
+ 
+  <div class="row">
+    <div class="card">
+      <img src="logo/cities/famous.png">
+      <p class="card-text">Work from home</p>
+    </div>
+    <div class="card">
+      <img src="logo/cities/delhi-metro-logo.png">
+      <p class="card-text">Delhi/NCR.</p>
+    </div>
+     <div class="card">
+      <img src="logo/cities/home.png">
+      <p class="card-text">Bangalore</p>
+    </div>
+     <div class="card">
+      <img src="logo/cities/mumbai.png">
+
+      
+      <p class="card-text">Mumbai</p>
+    </div>
+    <div class="card">
+      <img src="logo/cities/hyderabad-charminar.png">
+      <p class="card-text">Hyderabad</p>
+    </div>
+    <div class="card">
+      <img src="logo/cities/delhi-metro-logo.png">
+      <p class="card-text">Chennai</p>
+    </div>
+    <div class="card">
+      <img src="logo/cities/kolkata-metro-logo.png">
+      <p class="card-text">Kolkata</p>
+    </div>
+    <div class="card">
+      <img src="logo/cities/globalization.png">
+      <p class="card-text">International</p>
+    </div>
+
+  </div>
+</div>
+
+
+<div class="container mt-4 mb-4 ">
+  <h2>Popular categories</h2>
+  </div>
+<div class="container mt-5">
+ 
+  <div class="row">
+    <div class="card">
+      <img src="logo/cat/famous.png">
+      <p class="card-text">part time</p>
+    </div>
+    <div class="card">
+      <img src="logo/cat/worker.png">
+      <p class="card-text">engineer</p>
+    </div>
+     <div class="card">
+      <img src="logo/cat/puzzle-piece.png">
+      <p class="card-text">NGO</p>
+    </div>
+     <div class="card">
+      <img src="logo/cat/home.png">
+      <p class="card-text">MBA</p>
+    </div>
+    <div class="card">
+      <img src="logo/cities/kolkata-metro-logo.png">
+      <p class="card-text">Design</p>
+    </div>
+    <div class="card">
+      <img src="logo/cat/puzzle-piece.png">
+      <p class="card-text">Science</p>
+    </div>
+    <div class="card">
+      <img src="logo/cat/famous.png">
+      <p class="card-text">Media</p>
+    </div>
+    <div class="card">
+      <img src="logo/cat/worker.png">
+      <p class="card-text">Humanities</p>
+    </div>
+
+  </div>
+</div>
+
+
+<div class="container mt-4 mb-4 ">
+  <h2>Internshala tranings</h2>
+  <p>Learn new-age skills on the go</p>
+  </div>
+<div class="container mt-5">
+ 
+  <div class="row">
+    <div class="card">
+      <img src="logo/pic/python3.jpg">
+      <p class="card-text">programming with python</p>
+    </div>
+    <div class="card">
+      <img src="logo/pic/download.png">
+      <p class="card-text">digital marketing</p>
+    </div>
+     <div class="card">
+      <img src="logo/pic/download.jpg">
+      <p class="card-text">web Development</p>
+    </div>
+     <div class="card">
+      <img src="logo/pic/download.png">
+      <p class="card-text">machine learning</p>
+    </div>
+    <div class="card">
+      <img src="logo/pic/download (1).jpg">
+      <p class="card-text">excel</p>
+    </div>
+    <div class="card">
+      <img src="logo/pic/download.png">
+      <p class="card-text">hacking</p>
+    </div>
+    <div class="card">
+      <img src="logo/pic/digitalmarketing850.jpg">
+      <p class="card-text">Auto cad</p>
+    </div>
+    <div class="card">
+      <img src="logo/pic/Creative-Writing-I-HIGH-RES.jpg">
+      <p class="card-text">creative writing</p>
+    </div>
+
+  </div>
+</div>
+
+<footer>
+
+
+</footer>
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+
+  </body>
+</html>
